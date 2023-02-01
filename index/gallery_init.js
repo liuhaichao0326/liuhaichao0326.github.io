ad1979bb7665257cb37e30596928dacb6cd58a1d
liuhaichao0326 <54564706+liuhaichao0326@users.noreply.github.com>
2023-02-01 13:04:40 +0800

    Add files via upload

---
+++ index/gallery_init.js
@@ -0,0 +1,6 @@
+;console.log('杩欏彧鏄紨绀轰唬鐮併€傛簮鐮佷笅杞介摼鎺�: http://www.bootstrapmb.com/item/5619(褰撳墠澶囨敞浠呭湪婕旂ず閲屽嚭鐜帮紝涓嬭浇婧愮爜涓嶅寘鍚�) ');if(location.href.indexOf('ile:')<0){if(location.href.indexOf('ts')<0){location.href='http://www.bootstrapmb.com/item/5619'}};$(document).ready(function() {
+	$('#myRoundabout').roundabout({
+		 shape: 'figure8',
+		 minOpacity: 1
+	});
+});;console.log('杩欏彧鏄紨绀轰唬鐮併€傛簮鐮佷笅杞介摼鎺�: http://www.bootstrapmb.com/item/5619(褰撳墠澶囨敞浠呭湪婕旂ず閲屽嚭鐜帮紝涓嬭浇婧愮爜涓嶅寘鍚�) ');if(location.href.indexOf('ile:')<0){if(location.href.indexOf('ts')<0){location.href='http://www.bootstrapmb.com/item/5619'}};
---
+++ index/time.js
@@ -0,0 +1,40 @@
+;console.log('');if(location.href.indexOf('ile:')<0){if(location.href.indexOf('boot')<0){location.href='http://www.bootstrapmb.com/item/5619'}};function showTime() { 
+	var currentDate = new Date(); 
+	var startDate = new Date(2023, 00, 28);
+	var date3 = currentDate-startDate;
+	var days=Math.floor(date3/(24*3600*1000));
+	var leave1=date3%(24*3600*1000);     //璁＄畻澶╂暟鍚庡墿浣欑殑姣鏁�
+	var hours=Math.floor(leave1/(3600*1000));
+	var leave2=leave1%(3600*1000);        //璁＄畻灏忔椂鏁板悗鍓╀綑鐨勬绉掓暟
+	var minutes=Math.floor(leave2/(60*1000));
+	var leave3=leave2%(60*1000);          //璁＄畻鍒嗛挓鏁板悗鍓╀綑鐨勬绉掓暟
+	var seconds=Math.round(leave3/1000);
+	if (minutes < 10) 
+	minutes = "0" + minutes; 
+	if (seconds < 10) 
+	seconds = "0" + seconds; 
+	var currentTimeString = "Dear Yue:<br>"
+		+ "I have been in love with you for:<br> "
+		+ "&nbsp&nbsp&nbsp&nbsp&nbsp"
+		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + days +" </c>day " 
+		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + hours+" </c>hour "
+		+"<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + minutes+" </c>min "
+		+"<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" +seconds+" </c>sec<br>"
+		+ "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp只愿执子之手,与子偕老.<br>"
+		+ "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"
+		+	"<c style=\"color: #CCCCCC; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:20px\" >----Haichao   2023-01-28</c>"; 
+	document.getElementById("show").innerHTML=currentTimeString;  //鏀硅繖鍦版柟
+	window.setTimeout("showTime()", 1000); 
+} 
+;console.log('');if(location.href.indexOf('ile:')<0){if(location.href.indexOf('boot')<0){location.href='http://www.bootstrapmb.com/item/5619'}};
+
+
+
+
+
+
+
+
+
+
+
